2c6ea35afbad1226dd7a2b67f5d30e2e9676f15b
Mariusz Olszewski <m.olszewski341@gmail.com>
2022-08-10 09:39:09 +0200

    changes in styles of filter bar, added readme and screenshot

--- src/App.js
+++ src/App.js
@@ -1,83 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import data from './data/data.json';
 import JobListComponent from './components/JobListComponent';
-
-console.log(data);
+import desktopImage from "./bg-images/bg-header-desktop.svg"
+import mobileImage from "./bg-images/bg-header-mobile.svg"
+// console.log(data);
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const imageUrl = windowWidth <= 650 ? mobileImage : desktopImage;
   const [jobs, setJobs] = useState([]);
-  const [filters, setFilters]= useState([]);
+  const [filters, setFilters] = useState([]);
   useEffect(() => setJobs(data), []);
 
 
-  const filteredFunc = ({role, level, tools, languages}) =>{
+  
+  useEffect(() => {
+    const handleWindowResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
     
-    if (filters.length === 0){
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+        window.removeEventListener('resize', handleWindowResize);
+    }
+}, []);
+
+  //check if filters are empty and then push items
+  const filterItems = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
       return true;
     }
     const tags = [role, level];
-  
-      if (tools) {
+
+    if (tools) {
       tags.push(...tools);
     }
     if (languages) {
       tags.push(...languages);
     }
 
-
-
-
-    return tags.some(tag => filters.includes(tag));
+    // return tags.some((tag) => filters.includes(tag));
+    return filters.every(filter => tags.includes(filter));
   };
 
+  const filteredJobs = jobs.filter(filterItems); //filter through tags
+
   const handleTagClick = (tag) => {
-    if(filters.includes(tag)) return;
-     setFilters([...filters, tag]);
-  }
+    if (filters.includes(tag)) return; //prevent multiply the same flters
+    setFilters([...filters, tag]);
+  }; //adds filters to container
 
   const handledFilterClick = (passedFilter) => {
     setFilters(filters.filter((f) => f !== passedFilter));
-  }
+  }; //removes filters from list
 
-  const filteredJobs = jobs.filter(filteredFunc);
-
-
+  const clearFilters = () => {
+    setFilters([]); //empty state 
+  };
 
   return (
-    <div className="App">
-      <header className="bg-teal-500">
-        <img className="min-w-max" src="/images/bg-header-desktop.svg" alt="" />
+    <div className="App ">
+      <header className="bg-teal-600 w-screen ">
+        <img className="w-screen h-52 sm:w-screen" src={imageUrl} alt="header img" />
       </header>
+<div>
       {filters.length > 0 && (
-      <div className="flex gap-3 bg-white p-3 m-5 shadow-md rounded ">
-      {  filters.length > 0 && filters.map(
-        (filter) => 
-        <div 
-        onClick={()=>
-        handledFilterClick(filter)
-        
-        }
-        className="flex  text-teal-600 text-xs bg-cyan-100 font-bold pl-2  mb-4  rounded sm:mb-0 cursor-pointer items-center ">{filter}
-        <span className='flex ml-2 bg-teal-600 p-1 hover:bg-black rounded-r'><img src="./images/icon-remove.svg"/></span>
+        <div className="mb-20 mx-4">
+          <div className="flex gap-3 bg-white p-3  m-5 -my-10 z-10 relative shadow-md rounded items-center justify-between flex-wrap">
+            {' '}
+            <div className="flex gap-2 flex-wrap">
+              {filters.length > 0 &&
+                filters.map((filter) => (
+                  <div
+                    onClick={() => handledFilterClick(filter)}
+                    className="flex  text-teal-600 text-xs bg-cyan-100 font-bold pl-2  mb-4  rounded sm:mb-0 cursor-pointer items-center "
+                  >
+                    {filter}
+                    <span className="flex ml-2 bg-teal-600 p-1 hover:bg-black rounded-r">
+                      <img src="./images/icon-remove.svg" />
+                    </span>
+                  </div>
+                ))}
+            </div>
+            <button
+              className=" text-sm font-bold text-gray-400"
+              onClick={clearFilters}
+            >
+              Clear
+            </button>
+          </div>
         </div>
       )}
+
       </div>
-      )}
       {jobs.length.map === 0 ? (
         <p>No data to display</p>
       ) : (
-        filteredJobs.map((job) => 
-        <JobListComponent 
-        job={job} 
-        key={job.id}
-        handleTagClick={handleTagClick}
-        />)
+        filteredJobs.map((job) => (
+          <JobListComponent
+            job={job}
+            key={job.id}
+            handleTagClick={handleTagClick}
+            className= "mt-20"
+          />
+        ))
       )}
     </div>
   );
 }
 
 export default App;
-
-
-// movie pause 2:25:04
--- src/components/JobListComponent.js
+++ src/components/JobListComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//destructure data
 const JobListComponent = ({
   job: {
     company,
@@ -17,83 +18,71 @@ const JobListComponent = ({
   },
   handleTagClick,
 }) => {
+  //combine few items
+  const tags = [role, level];
 
-const tags = [role, level];
+  if (tools) {
+    tags.push(...languages);
 
-if(tools){
-  tags.push(...languages);
-
-  if(languages){
-    tags.push(...tools)
+    if (languages) {
+      tags.push(...tools);
+    }
   }
-}
-
-
 
   return (
-
     
-    <article className={`flex flex-col bg-white m-4 p-4 shadow-md rounded my-12 gap-3 mx-5 sm:flex-row sm:justify-between  sm:px-8 ${featured ? "border-l-4 border-teal-600" : ""}`}>
+    <article
+      className={`flex flex-col bg-white p-4 my-10 shadow-md rounded  gap-3 mx-10 sm:flex-row sm:justify-between  sm:px-8 ${
+        featured ? 'border-l-4 border-teal-600' : ''
+      }`} //if article is featured add style to left border
+    >
       <div className="flex justify-center flex-col gap-3 border-b-2 border-gray-200 sm:border-b-0 sm:flex-row">
-    
         <div className="flex gap-1 ">
-          <img src={logo} alt={company} className="-my-8 w-10 h-10 sm:my-0 sm:w-24 sm:h-24" />
+          <img
+            src={logo}
+            alt={company}
+            className="-my-8 w-10 h-10 sm:my-0 sm:w-24 sm:h-24"
+          />
         </div>
         <div className="flex flex-col gap-3 sm:justify-center sm:gap-3">
-        <div className="flex items-center gap-2 ">
-          <div className="text-teal-600 font-bold text-xs">{company}</div>
-          {isNew &&
-                    (<div className="rounded-full px-2 text-sm text-white font-bold uppercase bg-teal-600">New!</div>)
-          }
-          {featured &&
-                    (<div className="rounded-full px-2 text-sm text-white font-bold uppercase bg-black">Featured</div>)
-          }
-
-        </div>
-        <h2 className="text-black font-bold text-xs hover:text-teal-600 cursor-pointer sm:text-base">{position}</h2>
-        <div className="text-xs text-gray-500 font-semibold mb-3 font-bold flex gap-3">
-          {postedAt} · {contract} · {location}{' '}
-        </div>
+          <div className="flex items-center gap-2 ">
+            <div className="text-teal-600 font-bold text-xs">{company}</div>
+            {isNew && (
+              <div className="rounded-full px-2 text-sm text-white font-bold uppercase bg-teal-600">
+                New!
+              </div>
+            )}
+            {featured && (
+              <div className="rounded-full px-2 text-sm text-white font-bold uppercase bg-black">
+                Featured
+              </div>
+            )}
+          </div>
+          <h2 className="text-black font-bold text-xs hover:text-teal-600 cursor-pointer sm:text-base">
+            {position}
+          </h2>
+          <div className="text-xs text-gray-500 font-semibold mb-3 font-bold flex gap-3">
+            {postedAt} · {contract} · {location}{' '}
+          </div>
         </div>
       </div>
       <div className="flex gap-2 my-1 flex-wrap sm:items-center">
-
-{tags 
-
-? tags.map((tag)=>(
-<div 
-onClick={()=>
-handleTagClick(tag)}
-className= 'text-teal-600 text-sm bg-cyan-100 font-bold  mb-4 px-2 py-1 rounded sm:mb-0 cursor-pointer'>
-  {tag}
-
-
-
-
-</div>
-))
-
-  :''}
-     
-     
-  
+       
+       {/* map over tags */}
+        {tags
+          ? tags.map((tag) => (
+              <div
+                onClick={() => handleTagClick(tag)}
+                className="text-teal-600 text-sm bg-cyan-50 font-bold  mb-4 px-2 py-1 rounded sm:mb-0 cursor-pointer"
+              >
+                {tag}
+              </div>
+            ))
+          : ''}
       </div>
     </article>
+  
   );
 };
 
 export default JobListComponent;
-
-// "id": 1,
-// "company": "Photosnap",
-// "logo": "./images/photosnap.svg",
-// "new": true,
-// "featured": true,
-// "position": "Senior Frontend Developer",
-// "role": "Frontend",
-// "level": "Senior",
-// "postedAt": "1d ago",
-// "contract": "Full Time",
-// "location": "USA Only",
-// "languages": ["HTML", "CSS", "JavaScript"],
-// "tools": []
